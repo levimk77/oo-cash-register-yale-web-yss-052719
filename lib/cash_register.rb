48abb38481136f57b06e3b305a9904643bca7d23
Elijah Florence <efloren@siue.edu>
2019-05-29 17:16:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+  attr_accessor :total, :discount
+  
+ 
+  def initialize(discount=nil)
+    @total = 0 
+    @discount = discount
+    @items = []
+  end
+  
+  def add_item(title, price, quantity=1)
+    quantity.times {@items << title}
+    @total = @total + price*quantity
+  end
+  
+  def apply_discount
+     if @discount == nil
+    "There is no discount to apply."
+    elsif
+    @total = (@total * (1 - @discount*0.01)).to_i
+    "After the discount, the total comes to $#{@total}."
+    end
+  end
+  
+  def items
+    @items
+  end
+  
+  def void_last_transaction
+    @total = @total - 
+  end
+  
+  
+  
+  
+end
 
